09528c3bafe4a8f88adbff109ca96c1706f7b757
Natalia <nataliamp87@gmail.com>
2018-06-11 22:18:59 -0400

    2 ejercicios mas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,14 @@
 //var output = computeAverageLengthOfWords('code', 'programs');
-/*
+
 //Escriba una función 'computeAverageLengthOfWords' que recibe dos strings y devuelve el promedio de la longitud de ambos strings.
-function computeAverageLengthOfWords(word1, word2) {
+computeAverageLengthOfWords = (word1, word2) => {
   let x = word1.length;
   let y = word2.length;
   let average = (x + y) / 2;
   return average;
 }
 
-var output = computeAverageLengthOfWords('code', 'programs');
+let output = computeAverageLengthOfWords('code', 'programs');
 console.log(output); // --> 6
 
 
@@ -17,7 +17,7 @@ console.log(output); // --> 6
 //var output = getNthElement([1, 3, 5], 1);
 //console.log(output); // --> 3
 
-function getNthElement(array, n) {
+getNthElement = (array, n) => {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === n); {
       return array[n]
@@ -31,20 +31,20 @@ console.log(output); // --> 3
 
 //convertDoubleSpaceToSingle
 //function convertDoubleSpaceToSingle(str) {
-function convertDoubleSpaceToSingle(str) {
+convertDoubleSpaceToSingle = (str) => {
   let singleSpace = str.split("  ");
   console.log(singleSpace);
   let newString = singleSpace.join(" ")
   return newString;
 }
 
-var output = convertDoubleSpaceToSingle("string  with  double  spaces");
-console.log(output); // --> "string with double spaces"
+let output = convertDoubleSpaceToSingle("string  with  double  spaces");
+console.log(output);
 
 
 
 //areValidCredentials
-function areValidCredentials(name, password) {
+areValidCredentials = (name, password) => {
   if (name.length >= 3 || password.length >= 8) {
     return true;
 
@@ -53,24 +53,25 @@ function areValidCredentials(name, password) {
 
   }
 }
-var output = areValidCredentials('Ritu', 'mylongpassword');
+let output = areValidCredentials('Ritu', 'mylongpassword');
 console.log(output); // --> true
 
 
 
 //calculateBillTotal
-var output = calculateBillTotal(20);
+let output = calculateBillTotal(20);
 console.log(output); // --> 24.9
-function calculateBillTotal(preTaxAndTipAmount) {
+calculateBillTotal = (preTaxAndTipAmount) => {
 
-  var salesTax = preTaxAndTipAmount * 0.095;
-  var tip = preTaxAndTipAmount * 0.15;
+  let salesTax = preTaxAndTipAmount * 0.095;
+  let tip = preTaxAndTipAmount * 0.15;
 
-  var total = preTaxAndTipAmount + salesTax + tip;
+  let total = preTaxAndTipAmount + salesTax + tip;
   return total;
 
 }
 
+//filterOddElements
 filterOddElements = (arr) => {
 
   var newNumbers = [];
@@ -86,6 +87,34 @@ filterOddElements = (arr) => {
 let output = filterOddElements([1, 2, 3, 4, 5]);
 console.log(output);
 
-*/
 
+
+//findPairForSum
+findPairForSum = (array, number) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === number) {
+        return [array[i], array[j]];
+      }
+    }
+  }
+}
+
+let pair = findPairForSum([4, 34, 3, 12, 5, 2], 9);
+console.log(pair); // --> [4, 5]
+
+
+
+//computeSumOfAllElements
+computeSumOfAllElements = (arr) => {
+  let suma = 0;
+  for (let i in arr) {
+    suma = suma + arr[i]
+  }
+  return suma;
+}
+
+
+let output = computeSumOfAllElements([1, 2, 3]);
+console.log(output); // --> 6
 
